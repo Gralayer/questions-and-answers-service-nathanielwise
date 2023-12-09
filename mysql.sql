958b24e2b60613cd9e541521a4ad891c85b8a593
Gralayer <wisenathaniel1998@gmail.com>
2023-12-08 22:51:56 -0500

    Added packages.json.

--- mysql.sql
+++ mysql.sql
@@ -1,39 +1,35 @@
 CREATE TABLE questions (
-  id INT unsigned NOT NULL, /* Make this auto-increment after loading data */
+  question_id INT unsigned AUTO_INCREMENT, /* Make this auto-increment after loading data */
   product_id INT unsigned NOT NULL,
-  body VARCHAR(255) NOT NULL,
-  date_written BIGINT unsigned NOT NULL,
+  question_body VARCHAR(255) NOT NULL,
+  question_date BIGINT unsigned NOT NULL,
   asker_name VARCHAR(63) NOT NULL,
   asker_email VARCHAR(63) NOT NULL,
-  reported BOOLEAN NOT NULL,
-  helpful INT unsigned NOT NULL,
-  PRIMARY KEY (id)
+  reported BOOLEAN NOT NULL DEFAULT (0),
+  question_helpfulness INT unsigned NOT NULL DEFAULT (0),
+  PRIMARY KEY (question_id)
 );
 
 CREATE TABLE answers (
-  id INT unsigned NOT NULL, /* Make this auto-increment after loading data */
+  answer_id INT unsigned NOT NULL AUTO_INCREMENT, /* Make this auto-increment after loading data */
   question_id INT unsigned NOT NULL,
   body VARCHAR(255) NOT NULL,
   date_written BIGINT unsigned NOT NULL,
   answerer_name VARCHAR(63) NOT NULL,
   answerer_email VARCHAR(63),
   reported BOOLEAN NOT NULL,
-  helpful INT unsigned NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (question_id) REFERENCES questions(id)
+  answer_helpfulness INT unsigned NOT NULL,
+  PRIMARY KEY (answer_id),
+  FOREIGN KEY (question_id) REFERENCES questions(question_id)
 );
 
 CREATE TABLE answers_photos (
-  id INT unsigned NOT NULL, /* Make this auto-increment after loading data */
+  photo_id INT unsigned NOT NULL AUTO_INCREMENT, /* Make this auto-increment after loading data */
   answer_id INT unsigned NOT NULL,
   `url` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (answer_id) REFERENCES answers(id)
+  PRIMARY KEY (photo_id),
+  FOREIGN KEY (answer_id) REFERENCES answers(answer_id)
 );
-
-CREATE INDEX idx_question_id ON answers (question_id);
-CREATE INDEX idx_answers_id ON answers_photos (answer_id);
-CREATE INDEX idx_product_id ON questions (product_id);
 
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\questions.csv'
 INTO TABLE questions
@@ -53,6 +49,6 @@ FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-ALTER TABLE questions AUTO_INCREMENT=3518964;
-ALTER TABLE answers AUTO_INCREMENT=6879307;
-ALTER TABLE answers_photos AUTO_INCREMENT=2063760;
+CREATE INDEX idx_question_id ON answers (question_id);
+CREATE INDEX idx_answers_id ON answers_photos (answer_id);
+CREATE INDEX idx_product_id ON questions (product_id);
